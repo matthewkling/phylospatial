960e22d8a274863a1b4af6ec7d8340ac1307e509
Matthew Kling <mattkling@gmail.com>
2024-12-13 17:31:37 -0500

    suggest tmap

--- tests/testthat/test-ps_canape.R
+++ tests/testthat/test-ps_canape.R
@@ -1,3 +1,3 @@
 test_that("ps_canape runs without error on example data", {
-      expect_no_error(ps_canape(moss_data("binary"), n_reps = 3, n_iterations = 3))
+      expect_no_error(ps_canape(ps_simulate(data_type = "binary"), n_reps = 3, n_iterations = 3))
 })
